dcc92ce4cc43175f1cbdff1f566da76ff0b64f1f
Israel <imga.buap15@gmail.com>
2023-02-18 04:03:45 -0600

    zweite

--- scripts/index.js
+++ scripts/index.js
@@ -10,12 +10,12 @@ const contador = document.createElement("p");
 const paquetes = [
     { id: 1, nombre: "Paquete 1", imagen: "../imagenes/e-commerce.jpg", precio: 6000, cantidad: 1},
     { id: 2, nombre: "Paquete 2", imagen: "../imagenes/galeria1.jpg", precio: 9000, cantidad: 1},
-    { id: 3, nombre: "Paquete 3", imagen: "../imagenes/web_portal_d.jpg", precio: 8000, cantidad: 1},
+    { id: 3, nombre: "Paquete 3", imagen: "../imagenes/principal.jpg", precio: 8000, cantidad: 1},
     { id: 4, nombre: "Paquete 4", imagen: "../imagenes/galeria2.jpg", precio: 10000, cantidad: 1},
     { id: 5, nombre: "Paquete 5", imagen: "../imagenes/galeria3.jpg", precio: 7000, cantidad: 1},
     { id: 6, nombre: "Paquete 6", imagen: "../imagenes/galeria4.jpg", precio: 5000, cantidad: 1},
-    { id: 7, nombre: "Paquete 7", imagen: "../imagenes/galeria5.jpg", precio: 2500, cantidad: 1},
-    { id: 8, nombre: "Paquete 8", imagen: "../imagenes/galeria5.jpg", precio: 3000, cantidad: 1},    
+    { id: 7, nombre: "Paquete 7", imagen: "../imagenes/images2.jpg", precio: 2500, cantidad: 1},
+    { id: 8, nombre: "Paquete 8", imagen: "../imagenes/galeria6.jpg", precio: 3000, cantidad: 1},    
 ]
 
 const carritodeCompras = [];
